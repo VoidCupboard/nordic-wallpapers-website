a5ced8639d1db1d23d3d0ceec098e330740d8972
sai nivas mangu <sainonbeat99@gmail.com>
2021-11-24 01:33:53 +0530

    seo things

---
+++ components/Meta.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import img from '../static/metaimg.png'
+
+export default function Meta () {
+    return (
+        <Helmet>
+            
+            <title>Nordic Wallpapers</title>
+            <meta name="title" content="Nordic Wallpapers" />
+            <meta name="description" content="A collection of 'norded' wallpapers that go well with the Nord color scheme." />
+            
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://nordicwallpapers.vercel.app/" />
+            <meta property="og:title" content="Nordic Wallpapers" />
+            <meta property="og:description" content="A collection of 'norded' wallpapers that go well with the Nord color scheme." />
+            <meta property="og:image" content={img} />
+            
+            
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://nordicwallpapers.vercel.app/" />
+            <meta property="twitter:title" content="Nordic Wallpapers" />
+            <meta property="twitter:description" content="A collection of 'norded' wallpapers that go well with the Nord color scheme." />
+            <meta property="twitter:image" content={img} />
+        </Helmet>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Footer from '../../components/Footer'
 import Hero from "../../components/Hero"
 import Navbar from '../../components/Navbar'
 import Walps from '../../components/Walps'
+import Meta from '../../components/Meta'
 import '../styles/master.css'
 
 const IndexPage = () => {
   return (
     <main>
+      <Meta />
       <Navbar />
       <Hero />
       <Walps />
