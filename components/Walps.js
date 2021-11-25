ef89238a8230d55a2ea47add761867085367df4d
sai nivas mangu <sainonbeat99@gmail.com>
2021-11-25 16:57:17 +0530

    img dynamic loading, not optimized

--- components/Walps.js
+++ components/Walps.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { walps } from "../data"; 
+import React, { useState, useEffect } from "react";
+const Axios = require('axios').default;
 
-export default function Walps () {
+function Walps () {            
+    const [walps, setWalps] = useState([])
+    useEffect(() => {
+        Axios.get("https://api.github.com/repos/linuxdotexe/nordic-wallpapers/contents/wallpapers")
+            .then((response) => {
+                setWalps(response.data)
+            })
+    })
     return (
         <div className="walpPageContainer" id="walps">
             <h2>Wallpapers in this collection</h2>
@@ -16,11 +23,11 @@ export default function Walps () {
             </p>
 
             <div className="wallpaper">
-                {walps.map((walp) => (
-                    <a href={walp} target="_blank" rel="noreferrer" className="walpContainer">
-                        <div key={walp}>
+                {walps.map((item) => (
+                    <a href={item.download_url} target="_blank" rel="noreferrer" className="walpContainer">
+                        <div key={item.name}>
                             <img
-                            src={walp}
+                            src={item.download_url}
                             alt="wallpaper"
                             />
                         </div>
@@ -31,3 +38,5 @@ export default function Walps () {
         </div>
     )
 }
+
+export default Walps;
