96650011879ac4f3c9a66e2b89ba98ab2fc42a6f
sai nivas mangu <sainonbeat99@gmail.com>
2021-11-25 20:54:42 +0530

    fixing stuff

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,8 @@
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import GatsbyMonogram from "../src/images/Gatsby-Monogram.svg"
+import { config } from '@fortawesome/fontawesome-svg-core'
 
 export default function Footer () {
     return (
@@ -8,10 +10,12 @@ export default function Footer () {
             <p className="footerContent">
                 Made with <FontAwesomeIcon icon={faHeart} color="#bf616a" /> and 
                 <span className="gatsby">
-                <a href="https://gatsby.com" target="_blank" rel="noreferrer"> Gatsby </a>
+                <a href="https://gatsby.com" target="_blank" rel="noreferrer">
+                    <img src={ GatsbyMonogram} alt="gatsby logo"/>
+                </a>
                 </span>
                 by 
-                <a href="https://github.com/linuxdotexe" target="_blank" rel="norefferer"> Sai Nivas Mangu </a>
+                <a href="https://github.com/linuxdotexe" target="_blank" rel="noreferrer"> Sai Nivas Mangu </a>
             </p>
         </div>
     )
