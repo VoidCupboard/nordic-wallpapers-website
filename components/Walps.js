0b095aed0561b0ebf43f5170d0912382dcbd4c32
AsianCat54x <0xsapphir3@gmail.com>
2021-11-27 11:58:09 +0530

    Allow users to download file

--- components/Walps.js
+++ components/Walps.js
@@ -1,17 +1,41 @@
-import React, { useState, useEffect } from "react";
+import {faDownload} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {useEffect, useState} from 'react';
+
 const Axios = require('axios').default;
 
-function Walps () {            
+const downloadFile =
+    (url) => {
+        const options = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+
+        const fileName = url.split("/").at(-1)
+
+        fetch(url, options).then(response => {
+            response.blob().then(blob => {
+                let url = window.URL.createObjectURL(blob);
+                let a = document.createElement('a');
+                a.href = url;
+                a.download = fileName;
+                a.click();
+            });
+        });
+    }
+
+function
+Walps() {            
     const [walps, setWalps] = useState([])
     useEffect(() => {
-        Axios.get("https://api.github.com/repos/linuxdotexe/nordic-wallpapers/contents/wallpapers")
+        Axios.get('https://api.github.com/repos/linuxdotexe/nordic-wallpapers/contents/wallpapers')
             .then((response) => {
                 setWalps(response.data)
             })
     })
     const walpLength = walps.length
     return (
-        <div className="walpPageContainer" id="walps">
+        <div className='walpPageContainer' id='walps'>
             <h2>Wallpapers in this collection</h2>
             <p>
                 Here are all the wallpapers in this repository.
@@ -19,20 +43,23 @@ function Walps () {
                 Click on one to redirect to the wallpaper and
                 right-click on the wallpaper to download it. If you want
                 to download the entire collection,
-                <a href="https://github.com/linuxdotexe/nordic-wallpapers" target="_blank" rel="noreferrer"> here </a>
+                <a href='https://github.com/linuxdotexe/nordic-wallpapers' target='_blank' rel='noreferrer'> here </a>
                 is a link
                 to the repository.
             </p>
 
-            <div className="wallpaper">
+            <div className='wallpaper'>
                 {walps.map((item) => (
-                    <a href={item.download_url} target="_blank" rel="noreferrer" className="walpContainer">
-                        <div key={item.name}>
+                    <a href={item.download_url} target='_blank' rel='noreferrer' className='walpContainer'>
+                        <div key={item.name} className='wallpaper-item'>
                             <img
                             src={item.download_url}
                             alt={item.name}
                             title={item.name}
                             />
+                            <div className="download" onClick={() => downloadFile(item.download_url)}>
+                                <FontAwesomeIcon icon={faDownload} color="#88C0D0" />
+                            </div>                        
                         </div>
                     </a>
                 ))}
